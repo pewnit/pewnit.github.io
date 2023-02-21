ef951960d45503329d7e3d94c327fa6bea1df6c6
Punit Daga <punitdaga31@gmail.com>
2023-02-21 14:30:03 -0500

    Changed the background colors to match the tone/logo

--- script.js
+++ script.js
@@ -4,17 +4,35 @@ const links = document.querySelectorAll("a");
 // Listen for mouseover event on each link
 links.forEach(link => {
   link.addEventListener("mouseover", () => {
-    // Set the background color of the body to black
-    document.body.style.backgroundColor = "black";
+    // Set the background color of the body based on the link id
+    switch(link.id) {
+      case "link1":
+        document.body.style.backgroundColor = "#00ac72";
+        break;
+      case "link2":
+        document.body.style.backgroundColor = "#2c2c2c";
+        break;
+      case "link3":
+        document.body.style.backgroundColor = "#0072b1";
+        break;
+      case "link4":
+        document.body.style.backgroundColor = "#1DA1F2";
+        break;
+      case "link5":
+        document.body.style.backgroundColor = "#CD201F";
+        break;
+      default:
+        document.body.style.backgroundColor = "#AAAAFF";
+    }
 
     // Set the color of all links to white, except for the currently hovered link
     links.forEach(otherLink => {
       if (otherLink !== link) {
-        otherLink.style.color = "black";
+        otherLink.style.color = document.body.style.backgroundColor;
       }
     });
 
-    // Set the color of the hovered link to black
+    // Set the color of the hovered link to white
     link.style.color = "white";
   });
 
